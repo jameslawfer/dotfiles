b209dcb3f437bc066883388c14bd8b336865b30f
James Lawira-Fernandez <jameslf9594@gmail.com>
2021-05-06 13:11:04 +1000

    add colour formatting functionality to kb layout in i3status

--- .config/i3/scripts/i3status_ext.sh
+++ .config/i3/scripts/i3status_ext.sh
@@ -7,12 +7,18 @@ do
   read line
   KBD=$(~/.config/i3/scripts/keyboard_layout.sh)
   KBDTEXT=" $KBD"
+  KBDCOLOUR="#FFFFFF"
+
+  if [ $KBD != 'US' ] ; then
+    KBDCOLOUR="#00FFFB"
+  fi
+
   if [ "$line" == '{"version":1}' ] || [ "$line" == "[" ] ; then
     echo "$line" || exit 1
   elif [[ "$COUNTER" -eq 0 ]] ; then
     COUNTER=$((COUNTER+1))
-    echo $line | sed 's/^,//' | jq --compact-output --raw-output --arg KBDSTATUS "$KBDTEXT" '.[0:length-2] +[{"name":"keyboard","instance":"unknown","markup":"none","full_text":$KBDSTATUS}] + .[length-2:length]' || exit 1
+    echo $line | sed 's/^,//' | jq --compact-output --raw-output --arg KBDTEXT "$KBDTEXT" --arg KBDCOLOUR "$KBDCOLOUR" '.[0:length-2] +[{"name":"keyboard","instance":"unknown","color":$KBDCOLOUR,"markup":"none","full_text":$KBDTEXT}] + .[length-2:length]' || exit 1
   else
-    echo $line | sed 's/^,//' | jq --compact-output --raw-output --arg KBDSTATUS "$KBDTEXT" '.[0:length-2] +[{"name":"keyboard","instance":"unknown","color":"#00FFFB","markup":"none","full_text":$KBDSTATUS}] + .[length-2:length]' | sed 's/^/,/' || exit 1
+    echo $line | sed 's/^,//' | jq --compact-output --raw-output --arg KBDTEXT "$KBDTEXT" --arg KBDCOLOUR "$KBDCOLOUR" '.[0:length-2] +[{"name":"keyboard","instance":"unknown","color":$KBDCOLOUR,"markup":"none","full_text":$KBDTEXT}] + .[length-2:length]' | sed 's/^/,/' || exit 1
   fi
 done
--- .config/i3/scripts/keyboard_layout.sh
+++ .config/i3/scripts/keyboard_layout.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Depends on xkb-switch
 
-echo $(xkb-switch) | tr 'a-z' 'A-Z'
+echo $(xkb-switch -p) | tr 'a-z' 'A-Z'
