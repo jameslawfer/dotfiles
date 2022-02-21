ea4158d7c19213f7e61d2e173cf0298326536f82
James Lawira-Fernandez <jameslf9594@gmail.com>
2022-02-21 18:35:32 +1100

    fix multi-line bluetooth status

--- .config/i3/scripts/bluetooth_connections.sh
+++ .config/i3/scripts/bluetooth_connections.sh
@@ -3,8 +3,17 @@
 
 # modified one-liner from https://superuser.com/questions/1500383/bluetoothctl-list-connected-devices
 
-bluetoothctl devices | cut -f2 -d' ' | while read uuid; do
+declare -a bt_names
+COUNTER=0
+
+while read uuid; do
     if [[ $(bluetoothctl info $uuid | sed -rn 's/^\s*Connected:\s(.*)/\1/p') == "yes" ]]; then
-        echo "$(bluetoothctl info $uuid | sed -rn 's/^\s*Alias:\s(.*)/\1/p')"
+        if [[ ! $COUNTER -eq 0 ]] ; then
+            bt_names+=", "
+        fi
+        COUNTER=$((COUNTER+1))
+        bt_names+="$(bluetoothctl info $uuid | sed -rn 's/^\s*Alias:\s(.*)/\1/p')"
     fi
-done
+done < <(bluetoothctl devices | cut -f2 -d' ')
+
+echo $bt_names
